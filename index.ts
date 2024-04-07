be95dede5d39a8d02892603a684da8c15ceccd57
PaccyC <abayisengaaimepacifique@gmail.com>
2024-04-07 09:21:29 +0200

    added API for creating a reminder

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 
 import express from 'express'
-
+import remindersRouter from './routers/reminders'
 const app= express();
 
+
+app.use(express.json())
+app.get("/",(req,res)=>{
+    console.log("Home route")
+    res.send("Home route")
+}
+)
+
+app.use("/reminders",remindersRouter)
+
 app.listen(8000,()=>{
-    console.log("Server started");
+    console.log("Server started...");
     
-})
+})  
